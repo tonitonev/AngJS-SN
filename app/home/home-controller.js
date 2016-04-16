daf1d2c1fabed84101bbd4bfb815b55bd54f96c7
tonitonev <antoan_tonev@yahoo.com>
2016-04-16 13:49:32 +0300

    update 16.04.2016

--- app/home/home-controller.js
+++ app/home/home-controller.js
@@ -1,11 +1,27 @@
-angular.module('socialNetwork.home', [])
+angular.module('socialNetwork.home', [
+    'socialNetwork.users.authentication'
+])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {
             templateUrl: 'app/home/home.html',
             controller: 'HomeCtrl'
         });
     }])
-    .controller('HomeCtrl', ['$scope', function ($scope) {
-        $scope.hello = 'ZDRASTI!';
+    .controller('HomeCtrl', [
+        '$scope',
+        'authentication',
+        function ($scope,authentication) {
+        $scope.login = function (user) {
+            //console.log(user);
+            authentication.loginUser(user);
+        };
+
+        $scope.register = function (user) {
+            //console.log(user);
+            authentication.registerUser(user)
+                .then(function(registeredUser){
+                console.log(registeredUser);
+                });
+        }
     }
     ]);
