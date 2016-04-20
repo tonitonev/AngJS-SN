29ec42a7c3915c6e3596e1b9b11fb757b47e3fe6
tonitonev <antoan_tonev@yahoo.com>
2016-04-20 23:54:37 +0300

    update 20.04.2016

---
+++ app/common/footer-directive.js
@@ -0,0 +1,8 @@
+angular.module('socialNetwork.common.footer', [])
+    .directive('footerDirective', [function () {
+        return {
+            restrict: 'A',
+            templateUrl: ''
+        };
+    }
+    ]);
